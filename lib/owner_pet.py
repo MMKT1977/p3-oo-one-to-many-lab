5738eb8c758498ea047833eca13c235d395d5eee
MMKT1977 <kennedy.moranga@student.moringaschool..com>
2025-04-10 21:03:04 +0300

    Pet class implemented

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,19 @@
 class Pet:
-    pass
+
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all=[]
+
+    def __init__(self,name, pet_type, owner=None):
+        if pet_type not in self.PET_TYPES:
+            raise Exception(f"{pet_type} is invalid, must be one of {self.PET_TYPES}")
+        
+        self.name =name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name= name
+    
